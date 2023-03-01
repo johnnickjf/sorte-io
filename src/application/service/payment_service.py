4b8cddb202ca47aa9ffab80996a14bbe326ceb06
Johnnick F. Landim <johnnick.landim@gmail.com>
2023-03-01 01:39:22 -0300

    fix: Tirei uma duplicidade na validação da função create_payment.

--- src/application/service/payment_service.py
+++ src/application/service/payment_service.py
@@ -13,8 +13,6 @@ class PaymentService:
 
     def create_payment(self, payment: Payment) -> Payment:
         lottery = LotteryService(self.db).select_lottery(payment.lottery)
-        if not lottery:
-            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Lottery not found")
         if lottery.status != 1:
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Lottery closed")
         payment = Payment(**payment.dict(), price=float(lottery.price * payment.qtd))
